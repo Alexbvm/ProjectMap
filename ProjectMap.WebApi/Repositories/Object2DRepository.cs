bc2f1de15a1ed283429e8de4655b1626c595698f
Alexbvm <almaas@home.nl>
2025-03-12 13:59:51 +0100

    Poging 3 updaten

--- ProjectMap.WebApi/Repositories/Object2DRepository.cs
+++ ProjectMap.WebApi/Repositories/Object2DRepository.cs
@@ -4,7 +4,7 @@ using ProjectMap.WebApi.Models;
 
 namespace ProjectMap.WebApi.Repositories
 {
-    public class Object2DRepository
+    public class Object2DRepository : IObject2DRepository
     {
         private readonly string sqlConnectionString;
 
@@ -13,7 +13,7 @@ namespace ProjectMap.WebApi.Repositories
             this.sqlConnectionString = sqlConnectionString;
         }
 
-        public async Task<Object2D> InsertAsync(Object2D object2D, string environmentID)
+        public async Task<Object2D> InsertAsync(Object2D object2D)
         {
             using (var sqlConnection = new SqlConnection(sqlConnectionString))
             {
@@ -22,19 +22,19 @@ namespace ProjectMap.WebApi.Repositories
             }
         }
 
-        public async Task<Object2D?> ReadAsync(Guid id)
-        {
-            using (var sqlConnection = new SqlConnection(sqlConnectionString))
-            {
-                return await sqlConnection.QuerySingleOrDefaultAsync<Object2D>("SELECT * FROM [Object2D] WHERE Id = @Id", new { id });
-            }
-        }
+        //public async Task<Object2D?> ReadAsync(Guid id)
+        //{
+        //    using (var sqlConnection = new SqlConnection(sqlConnectionString))
+        //    {
+        //        return await sqlConnection.QuerySingleOrDefaultAsync<Object2D>("SELECT * FROM [Object2D] WHERE Id = @Id", new { id });
+        //    }
+        //}
 
-        public async Task<IEnumerable<Object2D>> ReadAsync()
+        public async Task<IEnumerable<Object2D>> ReadAsync(Guid environmentID)
         {
             using (var sqlConnection = new SqlConnection(sqlConnectionString))
             {
-                return await sqlConnection.QueryAsync<Object2D>("SELECT * FROM [Object2D]");
+                return await sqlConnection.QueryAsync<Object2D>("SELECT * FROM [Object2D] WHERE environmentID = @environmentID", new { environmentID});
             }
         }
         public async Task UpdateAsync(Object2D environment)
